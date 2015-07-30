a3c0051b6330c8127a478861a520528ca5ab588c
unknown <mic.rodneysantos@gmail.com>
2015-07-30 11:35:24 +0800

    updated assets

---
+++ resources/views/partials/css.blade.php
@@ -0,0 +1,10 @@
+    {{-- css vendor --}}
+    <link href="{{ asset('/css/vendor/font-awesome.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/vendor/nivo-slider.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/vendor/nivo-slider.default.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/vendor/etalage.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/vendor/dropzone.min.css') }}" rel="stylesheet">
+
+    {{-- css style --}}
+    <link href="{{ asset('/css/global.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
