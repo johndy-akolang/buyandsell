a38bbbda00d46f73b56f867b08e06d74e599b370
unknown <infantado.markdonnie@gmail.com>
2015-11-13 10:11:29 +0800

    revised meta tags

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,7 +9,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <meta name="title" content="Koll.com.ph - Philippines buy and sell website">
-    <meta name="description" content="#1 Buy and Sell Website, Philippines - Sell your 2nd hand items on KOLL.com.ph, buy and sell website. Post free online ads of your used car, mobile phone, furniture, and more.">
+    <!-- <meta name="description" content="#1 Buy and Sell Website, Philippines - Sell your 2nd hand items on KOLL.com.ph, buy and sell website. Post free online ads of your used car, mobile phone, furniture, and more."> -->
     <meta name="keywords" content="#1 Buy and Sell Website, Philippines, 2nd Hand for sale, Vehicles, Fashion, Pets, Jobs, Electronics, Real Estate, Sports, Toys, Others, For Sale Items, Low Price, Discount">
 
     {{-- title --}}
