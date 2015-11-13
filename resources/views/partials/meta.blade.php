06c1de3a3e18e799cafaf0eb9d4fd00e0fffcbe3
unknown <infantado.markdonnie@gmail.com>
2015-11-13 17:03:17 +0800

    add secure url for images share

--- resources/views/partials/meta.blade.php
+++ resources/views/partials/meta.blade.php
@@ -7,7 +7,7 @@
 @extends('app')
 @section('title', $items->title . ' | KOLL.com.ph')
 @section('og-title', $items->title)
-@section('og-image', $items->images)
+@section('og-image', secure_url( $items->images ))
 @section('og-description', $items->description)
 
 <!-- @section('og-image', secure_url( $items->images )) -->
