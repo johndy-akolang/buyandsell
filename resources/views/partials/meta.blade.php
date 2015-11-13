cf1a396042612218b4c11ef30e9ef49e2cc454f6
unknown <infantado.markdonnie@gmail.com>
2015-11-13 10:40:49 +0800

    remove image secure url

--- resources/views/partials/meta.blade.php
+++ resources/views/partials/meta.blade.php
@@ -7,10 +7,10 @@
 @extends('app')
 @section('title', $items->title . ' | KOLL.com.ph')
 @section('og-title', $items->title)
-@section('og-image', secure_url( $items->images ))
+@section('og-image', $items->images)
 @section('og-description', $items->description)
 
-
+<!-- @section('og-image', secure_url( $items->images )) -->
 
 
 
