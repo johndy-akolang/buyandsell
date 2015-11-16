ddafb10ad8d82d7148af0b557988679e6039b4dc
unknown <infantado.markdonnie@gmail.com>
2015-11-16 17:31:27 +0800

    add meta tags line preg_replace

--- resources/views/partials/meta.blade.php
+++ resources/views/partials/meta.blade.php
@@ -8,7 +8,8 @@
 @section('title', $items->title . ' | KOLL.com.ph')
 @section('og-title', $items->title)
 @section('og-image', secure_url( $items->images ))
-@section('og-description', $items->description)
+@section('og-description', preg_replace( "/\r|\n/", "", $items->description))
+@stop
 
 <!-- @section('og-image', secure_url( $items->images )) -->
 
