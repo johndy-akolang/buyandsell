631eaaf2da87b435ae9ba4dbc24aea21d5098812
unknown <infantado.markdonnie@gmail.com>
2015-11-20 14:24:35 +0800

    remove secure images url

--- resources/views/partials/meta.blade.php
+++ resources/views/partials/meta.blade.php
@@ -1,20 +1,11 @@
 
-<!-- Facebook meta tags -->
-<!-- <meta property="og:title" content="{{ $items->title }}" />
-<meta property="og:image" content"{{ $items->images }}" />
-<meta property="og:description" content"{{ $items->description }}" /> -->
 
 @extends('app')
 @section('title', $items->title . ' | KOLL.com.ph')
 @section('og-title', $items->title)
-@section('og-image', secure_url( $items->images ))
+@section('og-image', $items->images)
 @section('og-description', preg_replace( "/\r|\n/", "", $items->description))
-@stop
-
 <!-- @section('og-image', secure_url( $items->images )) -->
-
-
-
 
 
 
