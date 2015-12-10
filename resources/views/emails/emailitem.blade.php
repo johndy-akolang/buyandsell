4861376649f3c4f2f057efd7fa481f679169ae98
unknown <infantado.markdonnie@gmail.com>
2015-12-10 14:32:01 +0800

    template emails

---
+++ resources/views/emails/emailitem.blade.php
@@ -0,0 +1,24 @@
+<div class="" style="margin: 0;width: 550px;" >
+	<div class="" style="height: 50px;background: #4fc34f;width: 100%;">
+		<img src="{{ asset('/images/logo-koll.png') }}" />
+	</div>
+
+	<h3>Hello,</h3>
+
+	<p style="margin: 10px 0">
+		Your Ads " {{ $title }} " are now posted on KOLL.com.ph.
+	</p>
+	<p style="margin: 10px 0">
+		You can share it with your friends to sell your item quickly!
+	</p>
+
+	<p style="margin: 10px 0">
+		Good luck with your transaction!
+	</p>
+
+	<p style="margin: 20px 0">
+		Your Buy and Sell Partner, <br/>
+		Admin
+	</p>
+
+</div>
