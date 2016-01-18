9a1cb57bfbf16e87ab1e1dff9ffb0e6e61def964
unknown <infantado.markdonnie@gmail.com>
2016-01-18 10:30:18 +0800

    add facebook sdk

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -63,6 +63,30 @@
       js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=871265012896339";
       fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));</script>
+
+    <script>
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '1239890536027330',
+          xfbml      : true,
+          version    : 'v2.5'
+        });
+      };
+
+      (function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s); js.id = id;
+         js.src = "//connect.facebook.net/en_US/sdk.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));
+    </script>
+   <!--  <div
+  class="fb-like"
+  data-share="true"
+  data-width="450"
+  data-show-faces="true">
+</div> -->
 </body>
 <!-- Mark Donnie Infantado -->
 </html>
