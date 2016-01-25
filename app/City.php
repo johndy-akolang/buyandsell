cfc751619f6a1606a9119a5fb777c29309f449df
Christopher Rodney Santos <christopher.rodney.santos@gmail.com>
2016-01-25 09:44:38 +0800

    Annotate City model

--- app/City.php
+++ app/City.php
@@ -6,9 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class City extends Model
 {
+    /**
+     * Table used by the model
+     * @var string $table
+     */
     protected  $table = ['city'];
 
-
+    /**
+     * Attributes that are mass assignable
+     * @var array $fillable
+     */
     protected $fillable = [
         'citylist'
     ];
