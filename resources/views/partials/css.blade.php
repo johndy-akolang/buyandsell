552e6bab0590e975a99acf81823892d137314392
unknown <infantado.markdonnie@gmail.com>
2016-02-01 10:47:24 +0800

    Javascript/CSS fixed error file not found

--- resources/views/partials/css.blade.php
+++ resources/views/partials/css.blade.php
@@ -1,9 +1,9 @@
     {{-- css vendor --}}
     <link href="{{ asset('//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('/css/vendor/nivo-slider.css') }}" rel="stylesheet">
-    <link href="{{ asset('/css/vendor/nivo-slider.default.css') }}" rel="stylesheet">
-    <link href="{{ asset('/css/vendor/etalage.css') }}" rel="stylesheet">
-    <link href="{{ asset('/css/vendor/dropzone.css') }}" rel="stylesheet">
+    <!-- <link href="{{ asset('/css/vendor/nivo-slider.css') }}" rel="stylesheet"> 
+    <link href="{{ asset('/css/vendor/nivo-slider.default.css') }}" rel="stylesheet"> 
+    <link href="{{ asset('/css/vendor/etalage.css') }}" rel="stylesheet"> 
+    <link href="{{ asset('/css/vendor/dropzone.css') }}" rel="stylesheet"> -->
 
     {{-- css style --}}
     <link href="{{ asset('/css/global.css') }}" rel="stylesheet">
--- resources/views/partials/javascript.blade.php
+++ resources/views/partials/javascript.blade.php
@@ -4,9 +4,9 @@
 <script src="{{ asset('/js/vendor/bootstrap.min.js') }}" type="text/javascript"></script>
 <script src="{{ asset('/js/vendor/jquery.nivo.slider.js') }}" type="text/javascript"></script>
 <script src="{{ asset('/js/vendor/jquery.etalage.min.js') }}" type="text/javascript"></script>
-<script src="{{ asset('/js/vendor/modernizr.custom.js') }}" type="text/javascript"></script>
+<!-- <script src="{{ asset('/js/vendor/modernizr.custom.js') }}" type="text/javascript"></script> -->
 <script src="{{ asset('/js/vendor/jquery.pageparser-0.1.2.min.js') }}" type="text/javascript"></script>
-<script src="{{ asset('/js/vendor/dropzone.js') }}" type="text/javascript"></script>
+<!-- <script src="{{ asset('/js/vendor/dropzone.js') }}" type="text/javascript"></script> -->
 <script src="{{ asset('/js/vendor/jquery-maskmoney.js') }}" type="text/javascript"></script>
 
 {{-- javascript --}}
